7185b1e03e0852b2866a0794196999e7c549b703
DanielMueller139 <danielmueller-dm@gmx.de>
2021-02-05 14:28:52 +0100

    update loginbot

--- pythonbuch_aufgaben/Schmierblatt.py
+++ pythonbuch_aufgaben/Schmierblatt.py
@@ -1,3 +1,3 @@
 from selenium import webdriver
-browser = webdriver.Chrome('chromedriver.exe')
-browser.get('https://www.youtube.com/')
+browser = webdriver.Chrome(r"C:\Users\danie\AppData\Local\Programs\Python\chromedriver_win32\chromedriver.exe")
+browser.get('https://www.google.com/recaptcha/api2/demo')
