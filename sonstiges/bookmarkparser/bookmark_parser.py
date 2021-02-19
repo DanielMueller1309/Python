db6bd47badddea17f9a1629cb23e1a0bdcd53490
DanielMueller139 <danielmueller-dm@gmx.de>
2021-02-19 14:38:57 +0100

    first commit bookmark_parser.py

---
+++ sonstiges/bookmarkparser/bookmark_parser.py
@@ -0,0 +1,10 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Key
+
+# Pfad zum Bot
+data_path = r"/google_recaptcha_bot"
+
+# browser festlegen
+browser = webdriver.Chrome(r"C:\Users\danie\AppData\Local\Programs\Python\chromedriver_win32\chromedriver.exe")
