42b72091cfd8a5fa8ababb75725abfc50e72995b
DanielMueller1309 <danielmueller-dm@gmx.de>
2021-04-19 20:06:16 +0200

    test something

--- sonstiges/keylogger/keyboard.py
+++ sonstiges/keylogger/keyboard.py
@@ -2,6 +2,9 @@
 import time
 from pynput.keyboard import Key, Controller
 keyboard = Controller()
-time.sleep(2)
-keyboard.press(Key.cmd)
-keyboard.release(Key.cmd)
+#time.sleep(2)
+keyboard.press(Key.alt_l)
+keyboard.tap(Key.tab)
+
+#keyboard.press(Key.cmd)
+#keyboard.release(Key.cmd)
