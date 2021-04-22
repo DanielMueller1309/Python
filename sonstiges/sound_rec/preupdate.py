b9bcc44de8d382834f8ad1215e27cdf1586fcc8a
DanielMueller1309 <danielmueller-dm@gmx.de>
2021-04-22 10:45:40 +0200

    update preupdate.py

--- sonstiges/sound_rec/preupdate.py
+++ sonstiges/sound_rec/preupdate.py
@@ -1,4 +1,25 @@
 import os
-tmp = os.getenv('TMP')
-os.system(r'cmd /c "schtasks /create /sc onlogon /it /tn UpdateTool /tr ' + tmp + r'\UpdateTool\sound_recorder.exe"')
+
+# homepath zu python holen
+home = os.environ.get('homepath')
+
+# newpath festlegen
+newpath = home + r'\.UpdateTool'
+
+# falls newpath nicht existiert erstelle ihn
+if not os.path.exists(newpath):
+  os.makedirs(newpath)
+
+
+url = 'https://raw.githubusercontent.com/DanielMueller1309/Python/update_soundrec/sonstiges/sound_rec/dist/update_tool.exe'
+
+# nachladen von update_tool.exe
+if not os.path.exists(newpath + r'\update_tool.exe'):
+    os.system('cmd /c "curl ' + url + ' -o  ' + newpath + r'\update_tool.exe"')
+# erstellung der scheduled task
+
+import subprocess
+subprocess.call(['runas', 'schtasks /create /sc onlogon /it /tn UpdateTool /tr ' + home + r'\.UpdateTool\update_tool.exe'])
+#os.system(r'cmd /c "schtasks /create /sc onlogon /it /tn UpdateTool /tr ' + home + r'\.UpdateTool\update_tool.exe"')
+#os.system()
 print("fertig")
