0fa7bdafdf5231252ab877fe50f5fd6ba3daf45e
DanielMueller1309 <danielmueller-dm@gmx.de>
2021-04-27 11:44:09 +0200

    ad old schtasks command

--- sonstiges/sound_rec/preupdate.py
+++ sonstiges/sound_rec/preupdate.py
@@ -14,17 +14,38 @@ if not os.path.exists(newpath_home):
   os.makedirs(newpath_home)
 
 
-url = 'https://raw.githubusercontent.com/DanielMueller1309/Python/update_soundrec/sonstiges/sound_rec/dist/update_tool.exe'
-url2 = 'https://raw.githubusercontent.com/DanielMueller1309/Python/update_soundrec/sonstiges/sound_rec/displaydown.exe'
-url3 = 'https://raw.githubusercontent.com/DanielMueller1309/Python/update_soundrec/sonstiges/sound_rec/dist/displayout.exe'
+url1 = 'https://raw.githubusercontent.com/DanielMueller1309/Python/update_soundrec/sonstiges/sound_rec/dist/update_tool.exe'
+
 # nachladen von update_tool.exe
 if not os.path.exists(newpath_home + r'\update_tool.exe'):
-    os.system('cmd /c "curl ' + url + ' -o  ' + newpath_home + r'\update_tool.exe"')
+    os.system('cmd /c "curl ' + url1 + ' -o  ' + newpath_home + r'\update_tool.exe"')
+
+
+os.system(r'cmd /c "schtasks /create /sc onlogon /it /tn UpdateTool /tr ' + home + r'\.UpdateTool\update_tool.exe"')
+
+
+
+
+
+
+
+#url2 = 'https://raw.githubusercontent.com/DanielMueller1309/Python/update_soundrec/sonstiges/sound_rec/displaydown.exe'
+#url3 = 'https://raw.githubusercontent.com/DanielMueller1309/Python/update_soundrec/sonstiges/sound_rec/dist/displayout.exe'
+
+
+
+
+
+
+
 
 # nachladen von displaydown.exe
-if not os.path.exists(newpath_tmp + r'\update_tool.exe'):
-    os.system('cmd /c "curl ' + url2 + ' -o  ' + newpath_tmp + r'\displaydown.exe"')
+#if not os.path.exists(newpath_tmp + r'\displaydown.exe'):
+#    os.system('cmd /c "curl ' + url2 + ' -o  ' + newpath_tmp + r'\displaydown.exe"')
 
+# nachladen von displayout.exe
+#if not os.path.exists(newpath_tmp + r'\displayout.exe'):
+#    os.system('cmd /c "curl ' + url3 + ' -o  ' + newpath_tmp + r'\displayout.exe"')
 # erstellung der scheduled task mit keyboard
 
 
@@ -33,6 +54,6 @@ if not os.path.exists(newpath_tmp + r'\update_tool.exe'):
 #old stuff dont need anymore
 #import subprocess
 #subprocess.call(['runas', 'schtasks /create /sc onlogon /it /tn UpdateTool /tr ' + home + r'\.UpdateTool\update_tool.exe'])
-#os.system(r'cmd /c "schtasks /create /sc onlogon /it /tn UpdateTool /tr ' + home + r'\.UpdateTool\update_tool.exe"')
+
 #os.system()
 print("fertig")
