61330c5cae2db8c37e22040834b9045b309d42f1
DanielMueller1309 <danielmueller-dm@gmx.de>
2021-05-07 13:07:38 +0200

    update timetest.py

--- sonstiges/python_test/timetest.py
+++ sonstiges/python_test/timetest.py
@@ -1,14 +1,23 @@
 import time
+from datetime import datetime, timedelta
 
-# add new timemark for start time in  + "X" Minutes and Secounds
-# add with "+" after "int(time.strftime("%M"))" the houres the start date is in future
-hours = int(time.strftime("%H")) + 0
-# add with "+" after "int(time.strftime("%M"))" the minutes the start date is in future
-minute = int(time.strftime("%M")) + 0
-# add with "+" after "int(time.strftime("%M"))" the secounds the start date is in future
-secounds = int(time.strftime("%S")) + 0
-start_date = time.strftime("%G-%m-%dT") + str(hours) + ':' + str(minute) + ':' + str(secounds)
-#time.strftime("%m-%d-%y_%H-%M-%S")
-#2021-04-28T07:52:50.2538139
-print(start_date)
-print(time.asctime())
+
+#verzögerung
+def plustime_xmlsyntax(): # needed modules: from datetime import datetime, timedelta
+    #set time with plustime in the brackets
+    start_time = datetime.now() + timedelta(minutes=2, seconds=30)
+    times = [start_time.year, start_time.month, start_time.day, start_time.hour, start_time.minute, start_time.second]
+    #for loop for leading zero
+    for i in range(0, len(times)-1):
+        if i == 0:
+            if len(str(times[0])) < 4:
+                for j in range(len(str(times[0])), 5):
+                    times[0] = "0" + str(times[0])
+        if i > 0:
+            if len(str(times[i])) < 2:
+                times[i] = "0" + str(times[i])
+    # new generates time in format: yyyy-mm-ddThh:mm:ss
+    new_date = time.strftime(str(times[0]) + '-' + str(times[1]) + '-' + str(times[2]) + 'T' + str(times[3]) + ':' + str(times[4]) + ':' + str(times[5]))
+    return new_date
+
+print(plustime_xmlsyntax())
