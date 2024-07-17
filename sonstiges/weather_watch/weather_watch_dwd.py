2ba75a221df24ccc1f0d03eae7ca4ab8fdf7ee8e
DanielMueller1309 <danielmueller-dm@gmx.de>
2024-07-17 15:07:56 +0200

    anpassungen radar_images folder

--- sonstiges/weather_watch/weather_watch_dwd.py
+++ sonstiges/weather_watch/weather_watch_dwd.py
@@ -1,14 +1,23 @@
 import os
 import requests
 from datetime import datetime
+import argparse
+
+# Standardpfad ist das Home-Verzeichnis des Benutzers
+default_output_dir = os.path.expanduser("~/radar_images")
+
+# Argumentparser einrichten
+parser = argparse.ArgumentParser(description='Download weather radar images.')
+parser.add_argument('-o','--output_dir', type=str, default=default_output_dir,
+                    help='Directory to save the images. Default is the home directory of the user.')
+
+args = parser.parse_args()
+output_dir = args.output_dir
 
 # URL des Bildes
 image_url = "https://www.dwd.de/DWD/wetter/radar/rad_brd_akt.jpg"
 
-# Zielordner
-output_dir = "/home/danie/git/Python/sonstiges/weather_watch/radar_images"
-
-# Ordner erstellen, falls nicht vorhanden
+# Zielordner erstellen, falls nicht vorhanden
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
