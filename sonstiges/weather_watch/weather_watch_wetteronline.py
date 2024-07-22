3a1c9c42cfcbac8a8cbf55fb27cfd44834df633f
DanielMueller1309 <danielmueller-dm@gmx.de>
2024-07-22 12:17:02 +0200

    change sleep time, browser exec path and screenshot string

--- sonstiges/weather_watch/weather_watch_wetteronline.py
+++ sonstiges/weather_watch/weather_watch_wetteronline.py
@@ -22,26 +22,17 @@ if not os.path.exists(output_dir):
 # Zeitstempel für den Dateinamen
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 output_file = f"radar_image_{timestamp}.jpg"
-# Zielordner erstellen, falls nicht vorhanden
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
 
-# Zeitstempel für den Dateinamen
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-output_file = f"radar_image_{timestamp}.jpg"
 async def screenshot():
-    browser = await launch()
+    browser = await launch(executablePath='/usr/bin/chromium-browser')
     page = await browser.newPage()
     # Setze die Viewport-Größe, falls nötig
-    #w = 1280 * 1
-    #h = 800  * 1
     w = 2560
     h = 1600
     await page.setViewport({'width': w, 'height': h})
     await page.goto('https://radar.wo-cloud.com/desktop/wr/interactive?wrx=50.89,9.94&wrm=7&wrp=periodCurrentHighRes&wrf=true')
-    time.sleep(10)
-    await page.screenshot({'path': f'{os.path.join(output_dir, output_file)}'})
+    time.sleep(15)
+    await page.screenshot({'path': os.path.join(output_dir, output_file)})
     await browser.close()
-
 
 asyncio.get_event_loop().run_until_complete(screenshot())
